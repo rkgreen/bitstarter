411e14a4b295315ba8b67e7f4a5d54af0fa53596
John Smith <example@stanford.edu>
2013-07-12 04:03:07 +0000

    Update4 to web.js

--- web.js
+++ web.js
@@ -5,20 +5,19 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
 
-//    var buffer = new Buffer();
+//  var buffer = new Buffer(16);
     var fs = require('fs');
+    var buffer = new Buffer("Hello World from index.html", "utf-8")  
+      
+
 //    response.send(buffer.toString('utf-8', fs.readFileSync("index.html")));
 
- fs.readFileSync(index.html, function (err, data){
-//     if (err) throw err;
+ fs.readFileSync('index.html', function (err, data) {
+     if (err) throw err;
      console.log(data.toString());
 
-// var buffer = new Buffer("Hello World from index.html", "utf-8")
-
-
-
-
 });
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
